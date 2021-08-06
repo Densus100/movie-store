bd91f63bd3dc3e25176d4300d1019bf06dbc8258
densus100 <37259694+Densus100@users.noreply.github.com>
2021-08-06 13:27:05 +0700

    create-react-app base and install library
    npm install -save styled components
    npm install @material-ui/icons
    npm install @material-ui/core
    npm install react-router-dom

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+// npm install -save styled components
+// npm install @material-ui/icons
+// npm install @material-ui/core
+// npm install react-router-dom
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
